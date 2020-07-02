494bd6232dfd8737f9ff15e0664182c61add4b7b
richh73@gmail.com <richh73@gmail.com>
2020-07-01 22:44:37 -0400

    saving changes

--- src/Components/Admin/Learn/LearnForm.js
+++ src/Components/Admin/Learn/LearnForm.js
@@ -1,14 +1,54 @@
 import React, { Component } from "react";
-
+import TextEditor from "../../Helpers/TextEditor";
+import axios from "axios";
 class LeanrForm extends Component {
+  onChangeHandler = (event) => {
+    console.log(event.target.name, event.target.value);
+  };
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <p></p>
-        <label>Title</label>
-        <div>
-          <input type="text" name="title" />
+      <div className="admin-learn-form">
+        <p>Create a new dive package by filling out the form below.</p>
+        <div className="admin-learn-form-element">
+          <label>Title</label>
+          <div className="admin-learn-form-input-field">
+            <input type="text" name="title" onChange={this.onChangeHandler} />
+          </div>
         </div>
+        <div className="admin-learn-form-element">
+          <label>Description</label>
+          <div>
+            <TextEditor />
+          </div>
+        </div>
+        <div className="admin-learn-form-element">
+          <label>Price</label>
+          <div className="admin-learn-form-input-field">
+            <input type="text" name="price" onChange={this.onChangeHandler} />
+          </div>
+        </div>
+        <div className="admin-learn-form-element">
+          <label>Link</label>
+          <div className="admin-learn-form-input-field">
+            <input type="text" name="link" onChange={this.onChangeHandler} />
+          </div>
+        </div>
+        <div className="admin-learn-form-element">
+          <label>Link Text</label>
+          <div className="admin-learn-form-input-field">
+            <input
+              type="text"
+              name="linkText"
+              onChange={this.onChangeHandler}
+            />
+          </div>
+        </div>
+        <button>Submit</button>
       </div>
     );
   }
