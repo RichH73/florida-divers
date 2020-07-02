9f231a6cb7e2605d233ef12b74f27df48daf526e
richh73@gmail.com <richh73@gmail.com>
2020-07-01 21:36:54 -0400

    saving changes

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -4,47 +4,36 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actionCreators from "../../actions/index";
-import _ from 'lodash';
-import TextEditor from '../Helpers/TextEditor';
-
+import _ from "lodash";
+import TextEditor from "../Helpers/TextEditor";
+import LearnForm from "./Learn/LearnForm";
 
 class Admin extends Component {
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   changePanel = (event) => {
-    this.props.changeAdminPanel(event.target.id)
-  }
+    this.props.changeAdminPanel(event.target.id);
+  };
 
   learnPanel = () => {
-    return (
-      <div>
-        <label>Title</label>
-        <div><input type='text' name='title' /></div>
-      </div>
-    )
-  }
+    return <LearnForm />;
+  };
 
   galleryPanel = () => {
-    return (
-      <div>
-        Gallery Stuff
-      </div>
-    )
-  }
+    return <div>Gallery Stuff</div>;
+  };
 
   onSubmitHandler = () => {
     axios({
-      method: 'post',
-      url: 'http://floridivers.com:8600/newsLetter',
+      method: "post",
+      url: "http://floridivers.com:8600/newsLetter",
       data: {
         html: this.props.text,
-        name: 'Rich',
-        subject: 'Monthly News Letter'
-      }
-    })
-  }
+        name: "Rich",
+        subject: "Monthly News Letter",
+      },
+    });
+  };
 
   newsLetterPanel = () => {
     return (
@@ -52,52 +41,52 @@ class Admin extends Component {
         <TextEditor />
         <button onClick={this.onSubmitHandler}>Send</button>
       </div>
-    )
-  }
+    );
+  };
 
-  adminWelcome = () => {
-
-  }
+  adminWelcome = () => {};
 
   adminHeader = () => {
     return (
-      <div className='admin-header'>
+      <div className="admin-header">
         <ul>
-          <li id='learn' onClick={this.changePanel}>Learn</li>
-          <li id='gallery' onClick={this.changePanel}>Gallery</li>
-          <li id='letter' onClick={this.changePanel}>News Letter</li>
+          <li id="learn" onClick={this.changePanel}>
+            Learn
+          </li>
+          <li id="gallery" onClick={this.changePanel}>
+            Gallery
+          </li>
+          <li id="letter" onClick={this.changePanel}>
+            News Letter
+          </li>
         </ul>
       </div>
-    )
-  }
+    );
+  };
 
   adminWelcome = () => {
-    return (
-      <div style={{textAlign: 'center'}}>
-        Welcome!
-      </div>
-    )
-  }
+    return <div style={{ textAlign: "center" }}>Welcome!</div>;
+  };
 
   navigation = () => {
-    switch(this.props.page) {
-      case 'welcome':
+    switch (this.props.page) {
+      case "welcome":
         return this.adminWelcome();
-      case 'gallery':
-        return this.galleryPanel()
-      case 'letter':
-        return this.newsLetterPanel()
-      case 'learn':
-        return this.learnPanel()
+      case "gallery":
+        return this.galleryPanel();
+      case "letter":
+        return this.newsLetterPanel();
+      case "learn":
+        return <LearnForm />;
     }
-  }
+  };
 
   render() {
     const { packagesPrices } = this.props;
     return (
       <div className="admin">
         <this.adminHeader />
-        <div className='admin-panel-body'>
+        <div className="admin-panel-body">
           <this.navigation />
         </div>
       </div>
@@ -107,7 +96,7 @@ class Admin extends Component {
 
 const mapStateToProps = (state) => ({
   page: state.adminPanel.panel,
-  text: state.richText.text
+  text: state.richText.text,
 });
 
 const mapDispatchToProps = (dispatch) => {
---
+++ src/Components/Admin/Learn/LearnForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class LeanrForm extends Component {
+  render() {
+    return (
+      <div>
+        <p></p>
+        <label>Title</label>
+        <div>
+          <input type="text" name="title" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LeanrForm;
